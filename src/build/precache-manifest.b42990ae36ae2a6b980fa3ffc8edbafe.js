56ceef4791a769df261f66f6822c120b25c9af18
shashtrih <h.qaengineer@gmail.com>
2020-05-17 15:23:11 -0500

    First commit

---
+++ src/build/precache-manifest.b42990ae36ae2a6b980fa3ffc8edbafe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee47fb33f5f110a03e300493c604f436",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d29d665af9dedd6a6c08",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a865ea358e2942f5be2d",
+    "url": "/static/js/2.c1eea2a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c1eea2a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d29d665af9dedd6a6c08",
+    "url": "/static/js/main.16287591.chunk.js"
+  },
+  {
+    "revision": "45a69e7f68cbf8850338",
+    "url": "/static/js/runtime-main.13083a1f.js"
+  }
+]);
